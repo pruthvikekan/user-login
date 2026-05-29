d12a587d01d4040fe5baac65ed8428ca0a58be1a
pruthvikskc <pruthvik.skc@example.com>
2026-05-29 11:18:37 +0530

    fixed railway mysql connection

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://user-login-backend-production-8b3a.up.railway.app/api',
 });
 
 api.interceptors.request.use(
